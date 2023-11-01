3bce921e10b7f552c78709341141d395bbee2793
lenor4ik <leno4kamay08051987@gmail.com>
2023-11-01 21:29:07 +0100

    fix js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-var openButton = document.getElementById("menu-opn-btn");
-var closeButton = document.getElementById("menu-cls-btn");
-var targetDiv = document.getElementById("mob-menu");
+const openButton = document.getElementById("menu-opn-btn");
+const closeButton = document.getElementById("menu-cls-btn");
+const mobMenu = document.getElementById("menu-navigation");
+const mobMenuItems = mobMenu.querySelectorAll("li");
+const targetDiv = document.getElementById("mob-menu");
 
-
-openButton.addEventListener("click", function () {
+openButton.addEventListener("click", function() {
     targetDiv.classList.add("is-open");
 });
 
-closeButton.addEventListener("click", function () {
+closeButton.addEventListener("click", function() {
     targetDiv.classList.remove("is-open");
+});
+
+
+mobMenuItems.forEach(item => {
+item.addEventListener('click', function() {
+targetDiv.classList.remove("is-open");
+});
 });
