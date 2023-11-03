d0f8c7b0c805b249b0b927a6c340bdd19c6c3aeb
lenor4ik <leno4kamay08051987@gmail.com>
2023-11-03 10:06:19 +0100

    Fix mob-menu

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 const openButton = document.getElementById("menu-opn-btn");
+const openButtonSvg = document.getElementById("menu-opn-btn-svg");
+const openButtonUse = document.getElementById("menu-opn-btn-use");
 const closeButton = document.getElementById("menu-cls-btn");
 const mobMenu = document.getElementById("menu-navigation");
 const mobMenuItems = mobMenu.querySelectorAll("li");
@@ -14,7 +16,14 @@ closeButton.addEventListener("click", function() {
 
 
 mobMenuItems.forEach(item => {
-item.addEventListener('click', function() {
-targetDiv.classList.remove("is-open");
+    item.addEventListener('click', function() {
+    targetDiv.classList.remove("is-open");
+    });
 });
+
+document.addEventListener("click", function () { 
+    if (!mobMenu.contains(event.target) && event.target !== openButton && event.target !== openButtonSvg && event.target !== openButtonUse) { 
+    targetDiv.classList.remove("is-open")
+    }
 });
+
